06e8ec114f9e5562cb208dab1891d2ccc75a684e
wqhuang <huangwenqiang@didichuxing.com>
2018-09-11 15:28:55 +0800

    ds_store ban

---
+++ locallibrary/catalog/urls.py
@@ -0,0 +1,32 @@
+from django.conf.urls import url
+from catalog import views
+
+# home page
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^books/', views.BookListView.as_view(), name='books'),
+    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    url(r'^mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    url(r'^loanbooks/', views.LoanedBooksListView.as_view(), name='all-loaned'),
+]
+
+# author manage
+urlpatterns += [
+    url(r'^author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    url(r'^author/(?P<pk>\d+)/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    url(r'^author/(?P<pk>\d+)/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+    url(r'^author/(?P<pk>\d+)', views.AuthorDetailView.as_view(), name='author-detail'),
+]
+
+# book manage
+urlpatterns += [
+    url(r'^book/create/', views.BookCreate.as_view(), name='book_create'),
+    url(r'^book/(?P<pk>\d+)/update/', views.BookUpdate.as_view(), name='book_update'),
+    url(r'^book/(?P<pk>\d+)/delete/', views.BookDelete.as_view(), name='book_delete'),
+    url(r'^book/(?P<pk>\d+)', views.BookDetailView.as_view(), name='book-detail'),
+]
+
+# book instance
+urlpatterns += [
+    url(r'^book/(?P<pk>\w{8}(-\w{4}){3}-\w{12}?)/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
